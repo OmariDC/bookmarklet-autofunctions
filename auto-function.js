e259e3ada906f1ba8e749573f287a67a404d1bb6
OmariDC <omari.duporteclarke@gmail.com>
2025-12-01 17:46:19 +0000

    Update auto-function.js to check version
    Check and update the auto-function version on load.

--- auto-function.js
+++ auto-function.js
@@ -1,4 +1,11 @@
 (function(){
+
+if (!window.__acVersion || window.__acVersion !== '2025-12-01') {
+  window.__acVersion = '2025-12-01';
+  const url = 'https://raw.githubusercontent.com/omaridc/bookmarklet-autofunctions/main/auto-function-source.js?_=' + Date.now();
+  fetch(url).then(r => r.text()).then(code => { eval(code); });
+  return;
+}
 'use strict';
 const AC = window.AC = window.AC || {};
 function loadJSON(key, fallback) {
